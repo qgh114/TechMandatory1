30e9316aeb9d9b6e0349cc923cee47a04e8c95cc
SLL <qgh114@alumni.ku.dk>
2021-11-10 12:07:11 +0100

    added tls

--- src/main/java/edu/kea/kinoxp/config/ServerConfig.java
+++ src/main/java/edu/kea/kinoxp/config/ServerConfig.java
@@ -31,7 +31,7 @@ public class ServerConfig {
     private Connector redirectConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(9090);
         connector.setSecure(false);
         connector.setRedirectPort(443);
         return connector;
